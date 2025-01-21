8666a7884c38b1ff234f4237470e3a7dc0b75190
Sonic803 <sonic82003@gmail.com>
2025-01-21 12:21:38 +0100

    Added python test file

---
+++ python/test.py
@@ -0,0 +1,53 @@
+import json
+#open json file
+with open('../multicore_sched/simulations/test.json') as f:
+    data = json.load(f)
+
+keys=[i for i in data.keys()]
+
+d_vectors={}
+
+for key in keys:
+    
+    d_vectors[key]={}
+    vectors=data[key]['vectors']
+
+    for a in vectors:
+        print(f"{a['module']}, {a['name']}")
+        values=a['value']
+        mean=sum(values)/len(values)
+        print(mean)
+
+        module=a['module']
+        name = a['name']
+
+        d_vectors[key][module]={}
+        d_vectors[key][module][name]={}
+        d_vectors[key][module][name]['time']=a['time']
+        d_vectors[key][module][name]['value']=a['value']
+
+import matplotlib.pyplot as plt
+
+
+for key in d_vectors:
+    print(key)
+    time = d_vectors[key]['Computer.scheduler']['numProcReady:vector']['time']
+    value = d_vectors[key]['Computer.scheduler']['numProcReady:vector']['value']
+
+    # Calculate running average
+    cumulative_sum = 0
+    running_average = []
+    for i, v in enumerate(value):
+        cumulative_sum += v
+        running_average.append(cumulative_sum / (i + 1))
+
+    # Plot original values and running average
+    # plt.plot(time, value, label=f'{key} - {module} - {name} (Values)')
+    plt.plot(time, running_average, label=f'{key} - {module} - {name} (Avg)')
+
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('All Data and Running Averages')
+plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.show()
